685f9e9fe2462d83d5964a2b1e2e1d7f813888fc
Mike Jensen <mike.jensen.at@gmail.com>
2023-11-03 11:44:19 +0100

    Added contact information cards.
    + removed 2 redundant stylings.
    
    There is still no edit button. Not sure if it is even needed.

--- app/(tabs)/notice.tsx
+++ app/(tabs)/notice.tsx
@@ -1,29 +1,45 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, useColorScheme } from 'react-native';
+import { StyleSheet, TextInput, useColorScheme, SafeAreaView, ScrollView } from 'react-native';
 import { Text, View } from '../../components/Themed';
 
 export default function NoticeScreen() {
   const [houseRules, setHouseRules] = useState('');
 const colorScheme = useColorScheme();
+const users = [
+  { name: 'Test Bruger', phone: '12 34 56 78', email: 'testbruger42@gmail.com' },
+  { name: 'Andreas Alstrup', phone: '87 65 43 21', email: 'andreas.alstrup@gmail.com' },
+];
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Household Rules</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="There are no rules"
-        value={houseRules}
-        onChangeText={(text) => setHouseRules(text)}
-        editable
-        multiline
-      />
-      <Text style={styles.title}>Contact Information</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View>
+          <Text style={styles.title}>Household Rules</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Click to make rules"
+            value={houseRules}
+            onChangeText={(text) => setHouseRules(text)}
+            editable
+            multiline
+          />
+          <Text style={styles.title}>Contact Information</Text>
+          {users.map((user, index) => (
+            <View key={index} style={styles.userCard}>
+              <Text style={styles.userName}>{user.name}</Text>
+              <Text>Phone: {user.phone}</Text>
+              <Text>Email: {user.email}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 20,
@@ -31,18 +47,27 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     padding: 10,
     margin: 0,
-    width: '100%',
     color: 'white',
     backgroundColor: '#5CBCA9',
     borderTopWidth: 1,
     borderBotWidth: 1,
   },
   input: {
-    width: '100%',
     maxHeight: '50%',
     borderColor: 'gray',
     padding: 10,
     margin: 0,
     borderTopWidth: 1,
+  },
+  userCard: {
+    border: 1,
+    borderColor: 'gray',
+    backgroundColor: 'rgba(92, 188, 169, 0.27)',
+    padding: 10,
+    marginBottom: 10,
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
