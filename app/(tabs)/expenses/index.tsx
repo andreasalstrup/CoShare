fad2f5242e18fed879b8a24fcd532c4b2e89de23
Mike Jensen <mike.jensen.at@gmail.com>
2023-11-10 14:26:26 +0100

    Initial commit for showing the calculateexpneses

--- app/(tabs)/expenses/index.tsx
+++ app/(tabs)/expenses/index.tsx
@@ -1,32 +1,121 @@
 import { StyleSheet } from 'react-native';
+import { Text, View, } from '../../../components/Themed';
+import { Component } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { FontAwesome5 } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { calculateExpenses } from '../../../helpers/calculateExpenses';
 
-import EditScreenInfo from '../../../components/EditScreenInfo';
-import { Text, View } from '../../../components/Themed';
 
-export default function SettleScreen() {
+type Expense = {
+  user: string;
+  amount: number;
+};
+
+type Transaction = {
+  from: string;
+  to: string;
+  amount: number;
+};
+
+const DATA: Expense[] = [
+  { user: 'user 1', amount: 90 },
+  { user: 'user 2', amount: 30 },
+  { user: 'user 3', amount: 0 },
+];
+
+const calculatedExpenses = calculateExpenses(DATA);
+
+function renderItem({item, index}: { item: Transaction, index: number}) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Settle</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/expenses/index.tsx" />
+    <Swipeable
+      friction={1.5}
+      overshootFriction={8}
+      renderLeftActions={leftSwipeAction}
+      onSwipeableOpen={swipeHandler}>
+      <View style={[styles.container, 
+        {backgroundColor: index % 2 == 0 ? '#eeeeee' : '#D3D3D3'}]}>
+        <View style={styles.item}>
+          <Text style={styles.itemText}>{item.from}</Text>
+          <Text style={styles.itemAmount}>{item.amount} kr.</Text>
+          <Text style={styles.itemText}>{item.to}</Text>
+        </View>
+      </View>
+    </Swipeable>
+  )
+}
+
+function leftSwipeAction() {
+  return (
+    <View style={styles.leftSwipe}>
+      <FontAwesome5
+        name="arrow-alt-circle-right" 
+        size={32} 
+        color="white"
+        style={styles.swipeIcon}
+        />
     </View>
-  );
+  )
+}
+
+function swipeHandler(dir: 'left' | 'right') {
+  if (dir == "left") {
+      console.log(dir);
+  } else {
+      console.log(dir);
+  }
+}
+
+export default class SettleScreen extends Component {
+  render() {
+    return (
+      <FlatList
+        style={{marginTop: 48}}
+        data={calculatedExpenses}
+        renderItem={renderItem}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 48,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderBottomColor: '#000000',
+    padding: 2,
   },
-  title: {
-    fontSize: 20,
+  item: {
+    backgroundColor: 'transparent',
+    borderBottomColor: '#000000',
+    borderBottomWidth: 0,
+    padding: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  itemText: {
+    fontSize: 35,
+    color: 'black',
+    flexDirection: 'row',
+  },
+  itemAmount: {
+    fontSize: 35,
+    color: 'black',
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  infoText: {
+    fontSize: 12,
     fontWeight: 'bold',
+    color: 'black',
+    marginLeft: 10,
+    marginBottom: 5,
+    marginTop: -5,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  leftSwipe: {
+    backgroundColor: '#5CBCA9',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
   },
+  swipeIcon: {
+    padding: 20,
+  }
 });
--- helpers/calculateExpenses.tsx
+++ helpers/calculateExpenses.tsx
@@ -1,5 +1,5 @@
 type Expense = {
-    resident: string;
+    user: string;
     amount: number;
 };
 
@@ -10,34 +10,34 @@ type Transaction = {
 };
 
 export const calculateExpenses = (expenses: Expense[]): Transaction[] => {
-    // Calculate total spent by each resident
-    const totalSpent: { [name: string]: number } = {};
-    expenses.forEach((expense) => {
-        totalSpent[expense.resident] = (totalSpent[expense.resident] || 0) + expense.amount;
-    });
+// Calculate total spent by each user
+const totalSpent: { [name: string]: number } = {};
+expenses.forEach((expense) => {
+    totalSpent[expense.user] = (totalSpent[expense.user] || 0) + expense.amount;
+});
 
-    // Calculate the average amount spent by all residents
-    const numResidents = Object.keys(totalSpent).length;
-    const totalAmountSpent = Object.values(totalSpent).reduce((total, amount) => total + amount, 0);
-    const averageAmount = totalAmountSpent / numResidents;
+// Calculate the average amount spent by all users
+const numUsers = Object.keys(totalSpent).length;
+const totalAmountSpent = Object.values(totalSpent).reduce((total, amount) => total + amount, 0);
+const averageAmount = totalAmountSpent / numUsers;
 
-    // Calculate who owes or is owed how much
-    const transactions: Transaction[] = [];
-    Object.keys(totalSpent).forEach((creditor) => {
-        let amountOwed = totalSpent[creditor] - averageAmount;
+// Calculate who owes or is owed how much
+const transactions: Transaction[] = [];
+Object.keys(totalSpent).forEach((creditor) => {
+    let amountOwed = totalSpent[creditor] - averageAmount;
 
-        if (amountOwed > 0) {
-            Object.keys(totalSpent).forEach((debitor) => {
-                if (creditor !== debitor && totalSpent[debitor] < averageAmount) {
-                    const settleAmount = Math.min(amountOwed, averageAmount - totalSpent[debitor]);
-                    transactions.push({ to: creditor, from: debitor, amount: settleAmount });
-                    totalSpent[creditor] -= settleAmount;
-                    totalSpent[debitor] += settleAmount;
-                    amountOwed -= settleAmount;
-                }
-            });
-        }
-    });
+    if (amountOwed > 0) {
+        Object.keys(totalSpent).forEach((debitor) => {
+            if (creditor !== debitor && totalSpent[debitor] < averageAmount && averageAmount != totalSpent[creditor]) {
+                const settleAmount = Math.min(amountOwed, averageAmount - totalSpent[debitor]);
+                transactions.push({ to: creditor, from: debitor, amount: settleAmount });
+                totalSpent[creditor] -= settleAmount;
+                totalSpent[debitor] += settleAmount;
+                amountOwed -= settleAmount;
+            }
+        });
+    }
+});
 
-    return transactions;
+return transactions;
 };
