9c00ba75ef2d27df6de27def2204bc96d9a6cec2
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2023-11-29 17:16:59 +0100

    Added user hook

---
+++ hooks/useUser.ts
@@ -0,0 +1,46 @@
+interface IUser {
+    create(fullName: string, email: string, phoneNumber: string, password: string, login: (ack: any) => Boolean): Boolean;
+    login(phoneNumber: string, password: string, success: (ack: any, user: UserGunDB) => Boolean): Boolean;
+}
+
+class User implements IUser {
+    readonly user: UserGunDB;
+
+    constructor(private gun: Gun) {
+        this.user = this.gun.user();
+    }
+
+    public create(fullName: string, email: string, phoneNumber: string, password: string, login: (ack: any) => Boolean): Boolean {
+        user.create(phoneNumber, password, (ack: any) => {
+            if (ack.err != undefined) {
+                return false;
+            }
+
+            const newUser = gun.user(ack.pub);
+            newUser.get("fullName").put(fullName);
+            newUser.get("email").put(email);
+            this.user.auth(phoneNumber, password, login);
+        });
+
+        return true;
+    }
+
+    public login(phoneNumber: string, password: string, success: (ack: any, user: UserGunDB) => Boolean): Boolean {
+        this.user.auth(phoneNumber, password, (ack: any) => {
+            if (this.isLoggedIn()) {
+                success(ack, this.user);
+                return true;
+            }
+        });
+
+        return false;
+    }
+
+    private isLoggedIn(): boolean {
+        return this.user.is;
+    }
+}
+
+export function useUser(gun: Gun): IUser {
+    return new User(gun);
+}
