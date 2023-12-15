6998d174541cdd138abf8516db0f94eae6a6c729
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2023-12-15 17:54:18 +0100

    Fix meal plan

--- handlers/meal.ts
+++ handlers/meal.ts
@@ -1,6 +1,6 @@
 interface IMealPlan {
-    getWeekMealPlan(weekKey: string): Promise<WeekTexts>;
-    setWeekMealPlan(weekKey: string, dayMeals: WeekTexts): Promise<WeekTexts>;
+    getWeekMealPlan(weekKey: string, callback: (weekTexts: WeekTexts) => void): void;
+    setWeekMealPlan(weekKey: string, dayMeals: WeekTexts): void;
 }
 
 class MealPlanHandle implements IMealPlan {
@@ -8,62 +8,30 @@ class MealPlanHandle implements IMealPlan {
     groupId: string = "";
 
     constructor(private gun: Gun) {
-            this.user = gun.user();
-            this.user.get("group").on((data: any) => {
+            this.user = gun.user(userPub);
+            this.user.get("group").on((data: { groupId: string}) => {
                 this.groupId = data?.groupId.toString()     
             });
     }
 
-    public async getWeekMealPlan(weekKey: string): Promise<WeekTexts> {
-        await this.waitForId();
-
-        let dayMeals: WeekTexts = undefined;
-
-        const waitForweekText = async (): Promise<void> => {
-            return new Promise<void>((resolve) => {
-                this.gun.get("groups").get(this.groupId).get("mealplan").get(weekKey).open((data: WeekTexts) => {
-                    const check = () => {
-                        if (data || dayMeals) {
-                            dayMeals = data
-                            resolve();
-                        } else {
-                            dayMeals = {
-                                Mon: "",
-                                Tue: "",
-                                Wed: "",
-                                Thu: "",
-                                Fri: "",
-                                Sat: "",
-                                Sun: ""
-                            };
-                            setTimeout(check, 100);
-                        };
-                    };
-                    check();
-                });
-            });
-        };
-    
-        await waitForweekText();
-        return dayMeals;
-    }
-
-    public async setWeekMealPlan(weekKey: string, newDayMeals: WeekTexts): Promise<WeekTexts> {
-        await this.waitForId();
-        return await this.gun.get("groups").get(this.groupId).get("mealplan").get(weekKey).put(newDayMeals);
-    }
-
-    private async waitForId(): Promise<void> {
-        return new Promise<void>((resolve) => {
-            const checkId = () => {
-                if (this.groupId !== "") {
-                    resolve();
-                } else {
-                    setTimeout(checkId, 100);
-                }
-            };
-            checkId();
+    public getWeekMealPlan(weekKey: string, callback: (weekTexts: WeekTexts) => void): void {
+        this.gun.get("groups").get(this.groupId).get("mealplan").get(weekKey).open((data: WeekTexts) => {
+            callback(data);
         });
+
+        callback({
+            Mon: "",
+            Tue: "",
+            Wed: "",
+            Thu: "",
+            Fri: "",
+            Sat: "",
+            Sun: ""
+        });
+    }
+
+    public setWeekMealPlan(weekKey: string, weekTexts: WeekTexts): void {
+        this.gun.get("groups").get(this.groupId).get("mealplan").get(weekKey).put(weekTexts);
     }
 }
 
